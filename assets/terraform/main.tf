13950fd4afc0dc73920eb9216436fff4fa079eea
Stephen <51303308+thaengineer@users.noreply.github.com>
2022-05-17 18:55:44 -0400

    Update main.tf

--- assets/terraform/main.tf
+++ assets/terraform/main.tf
@@ -11,13 +11,13 @@ provider "azurerm" {
 }
 
 resource "azurerm_resource_group" "RgProd" {
-  name     = "${var.prefix}RgProd"
+  name     = "RgProd${var.extension}"
   location = var.location
 }
 
 resource "azurerm_virtual_network" "vNetProd" {
   name                = "vNetProd"
-  address_space       = ["10.1.0.0/16"]
+  address_space       = [var.address_space]
   location            = azurerm_resource_group.RgProd.location
   resource_group_name = azurerm_resource_group.RgProd.name
 }
@@ -26,7 +26,7 @@ resource "azurerm_subnet" "sNetProd" {
   name                 = "sNetProd"
   resource_group_name  = azurerm_resource_group.RgProd.name
   virtual_network_name = azurerm_virtual_network.vNetProd.name
-  address_prefixes     = ["10.1.0.0/24"]
+  address_prefixes     = [var.subnet_prefix]
 }
 
 resource "azurerm_network_interface" "NIC" {
@@ -47,9 +47,9 @@ resource "azurerm_windows_virtual_machine" "VirtualMachine" {
   name                = "VirtualMachine${count.index}"
   resource_group_name = azurerm_resource_group.RgProd.name
   location            = azurerm_resource_group.RgProd.location
-  size                = "Standard_D2as_v5"
-  admin_username      = "azadmin"
-  admin_password      = "Azure?123"
+  size                = var.vm_size
+  admin_username      = var.admin_username
+  admin_password      = var.admin_password
   network_interface_ids = [
     azurerm_network_interface.NIC[count.index].id,
   ]
@@ -60,9 +60,9 @@ resource "azurerm_windows_virtual_machine" "VirtualMachine" {
   }
 
   source_image_reference {
-    publisher = "MicrosoftWindowsServer"
-    offer     = "WindowsServer"
-    sku       = "2022-Datacenter"
-    version   = "latest"
+    publisher = var.image_publisher
+    offer     = var.image_offer
+    sku       = var.image_sku
+    version   = var.image_version
   }
 }
